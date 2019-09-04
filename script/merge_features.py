04d3e21a3b71ea6a348f66e224b7c76c546323f6
Martin Monperrus <monperrus@users.noreply.github.com>
2019-09-04 13:36:07 +0200

    Update merge_features.py

--- script/merge_features.py
+++ script/merge_features.py
@@ -17,7 +17,7 @@ output_path = os.path.realpath(sys.argv[2])
 
 output = []
 count = 0
-path_benchmark = os.path.join(output_path, benchmark)
+path_benchmark = output_path
 for file_name in sorted(os.listdir(path_benchmark)):
     file_path = os.path.join(path_benchmark, file_name)
     if not os.path.isfile(file_path):
@@ -25,9 +25,10 @@ for file_name in sorted(os.listdir(path_benchmark)):
     with open(file_path) as fd:
         data = json.load(fd)
         data['benchmark'] = benchmark
-        with codecs.open(os.path.join(root, "benchmark", benchmark, file_name.replace("_all.json", "").replace("_", "/"), "path.diff"), "r", encoding='utf8', errors='ignore') as fd:
+        project=file_name.split('_')[0]
+        with codecs.open(os.path.join(benchmark, project, file_name.replace("_all.json", ""), "path.diff"), "r", encoding='utf8', errors='ignore') as fd:
             data['diff'] = fd.read()
         output.append(data)
 
-with codecs.open(os.path.join(output_path, "%s.json" % benchmark), "w",encoding='utf8') as fd:
+with codecs.open(os.path.join("%s/dissection.json" % benchmark), "w",encoding='utf8') as fd:
     json.dump(output, fd)
